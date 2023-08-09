564f46bd02040c2b1610634e9bd13998347c3fc6
hamidyfine <hamid.yaftian@gmail.com>
2023-08-09 09:35:39 +0330

    use-form hook with useRef

--- src/hooks/use-form.tsx
+++ src/hooks/use-form.tsx
@@ -4,44 +4,31 @@ type TFormConfig<T> = {
     initial_value: T;
 };
 
-type TFormData = {
-    values: any;
-    errors: any;
-}
-
 interface FormSubmitHandler {
   (values: any, errors: any): void;
 }
 
 export const useForm = <T extends {}>({ initial_value }: TFormConfig<T>) => {
-    const [form_data, setFormData] = React.useState<TFormData>({
-        values: initial_value,
-        errors: {},
-    });
+    const valuesRef = React.useRef<T>(initial_value);
+    const errorsRef = React.useRef({});
 
     const onChange = React.useCallback(
         (e: React.ChangeEvent<HTMLInputElement>) => {
             const { name, value } = e.target;
-            const newValues = { ...form_data.values, [name]: value };
-            setFormData(prevState => {
-                return {
-                    ...prevState,
-                    values: newValues,
-                };
-            });
+            valuesRef.current = { ...valuesRef.current, [name]: value };
         },
-        [form_data.values],
+        [],
     );
 
     const onSubmit = (onSubmit: FormSubmitHandler) => async (event: React.FormEvent<HTMLFormElement>) => {
         event.preventDefault();
-        onSubmit(form_data.values, form_data.errors);
+        onSubmit(valuesRef.current, errorsRef.current);
     };
 
     return {
         onSubmit,
         onChange,
-        values: form_data.values,
-        errors: form_data.errors,
+        values: valuesRef.current,
+        errors: errorsRef.current,
     };
 };
